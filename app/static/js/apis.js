0fddd44f8c129c16564662a45dce44b69a113956
LimHM <gusals3407@gmail.com>
2017-03-22 22:24:26 +0900

    쿠키의 토큰을 이용해 post 생성 api 호출

--- app/static/js/apis.js
+++ app/static/js/apis.js
@@ -49,3 +49,20 @@ function obtainAuthToken(username, password) {
   .fail(function(data){
   });
 }
+
+
+function postCreate() {
+  var url = 'http://localhost:8000/api/post/';
+  var token = 'Token ' + getCookie('instagramToken');
+  console.log(token);
+  $.ajax({
+    url: url,
+    method: 'POST',
+    headers: {
+      Authorization: token,
+    }
+  })
+  .done(function(data){
+    console.log(data);
+  });
+}
--- app/static/js/cookie.js
+++ app/static/js/cookie.js
@@ -4,3 +4,16 @@ function setCookie(name, value, expireDays) {
   var c_value=escape(value) + ((expireDays==null) ? "" : "; expires="+exdate.toUTCString());
   document.cookie=name + "=" + c_value;
 }
+
+function getCookie(c_name) {
+	var i,x,y,ARRcookies=document.cookie.split(";");
+	for (i=0;i<ARRcookies.length;i++) {
+	  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+	  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+	  x=x.replace(/^\s+|\s+$/g,"");
+
+	  if (x==c_name) {
+			return unescape(y);
+		}
+	}
+}
